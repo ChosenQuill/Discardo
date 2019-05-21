298227c023c9e03937ce436d9460fb5ee9bad4c7
ChosenQuill <31707957+ChosenQuill@users.noreply.github.com>
2019-05-21 03:30:45 -0500

    Submitting and receiving works

--- back-end/src/main/java/com/suredroid/Controller.java
+++ back-end/src/main/java/com/suredroid/Controller.java
@@ -4,6 +4,7 @@ import org.joor.Reflect;
 import org.joor.ReflectException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,27 +15,30 @@ import java.util.function.Supplier;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+
+@CrossOrigin
 @RestController
 public class Controller {
     @RequestMapping("/submit")
     public ResponseEntity sumbit(@RequestParam(value="code", defaultValue = "") String code){
         Pattern className = Pattern.compile("class (\\w+)");
         try {
-            System.out.println(code);
             Matcher m = className.matcher(code);
             if(m.find()) {
                 Supplier<String> supplier = Reflect.compile(m.group(1),
                         code
                 ).create().get();
-                System.out.println(supplier.get());
-                return new ResponseEntity(HttpStatus.ACCEPTED);
+                return new ResponseEntity(supplier.get(),HttpStatus.ACCEPTED);
             }
             else
                 return new <String>ResponseEntity("Missing Class",HttpStatus.BAD_REQUEST);
         } catch (Exception e){
             if(e instanceof ReflectException){
                 return new <String>ResponseEntity(e.getLocalizedMessage(),HttpStatus.BAD_REQUEST);
-            } else {
+            } else if (e instanceof ClassCastException){
+                return new <String>ResponseEntity("Interface not implemented. You class does not implement the proper interface.",HttpStatus.BAD_REQUEST);
+            }
+            else {
                 StringWriter errors = new StringWriter();
                 e.printStackTrace(new PrintWriter(errors));
                 return new <String>ResponseEntity(errors.toString(),HttpStatus.INTERNAL_SERVER_ERROR);
@@ -47,3 +51,4 @@ public class Controller {
         return new <String>ResponseEntity("Status Ok.",HttpStatus.OK);
     }
 }
+
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,139 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {createMuiTheme, CssBaseline} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
+import {makeStyles, withStyles} from "@material-ui/styles";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogActions from "@material-ui/core/DialogActions";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+const theme = createMuiTheme();
+
+const AppStyles = {
+    textField: {
+        margin: theme.spacing(2),
+    }
+};
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        const {classes} = props;
+        this.classes = classes;
+        this.state = {
+            open: false,
+            message: {
+                title: "N/A",
+                text: "N/A",
+            }
+        };
+
+        this.default = "import java.util.function.Supplier;\n" +
+            "class test implements Supplier<String> {\n" +
+            "    @Override\n" +
+            "    public String get() {\n" +
+            "        return \"Something\";\n" +
+            "    }\n" +
+            "}";
+
+        this.submitCode = this.submitCode.bind(this);
+        this.closePopup = this.closePopup.bind(this);
+    }
+
+    submitCode() {
+        const code = document.getElementById("code").value;
+
+        if(code) {
+            const url = new URL("http://localhost:5000/submit");
+            url.searchParams.append("code", code);
+            fetch(url).then(response => {
+                // if (response.status === 200) {
+                return Promise.all([response.status, response.text()]);
+            }).then(promise => {
+                this.setState({
+                    message: {
+                        title: promise[0],
+                        text: promise[1],
+                    },
+                    open: true,
+                    error: false,
+                    helper: null
+                });
+                // }
+            }).catch((error)=>{
+                this.setState({
+                    message: {
+                        title: "Unknown Error while submitting code",
+                        text: "Server is most likely down. Error: " + error,
+                    },
+                    open: true,
+                    error: false,
+                    helper: null
+                });
+            });
+        } else {
+            this.setState({error: true, helper: "Please insert your code here."})
+        }
+    }
+
+    closePopup() {
+        this.setState({open: false});
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <CssBaseline/>
+                <Paper className="paper">
+                    <TextField
+                        id="code"
+                        label="Paste Code Here"
+                        multiline
+                        fullWidth
+                        rows={12}
+                        rowsMax={48}
+                        className={this.classes.textField}
+                        margin="normal"
+                        variant="outlined"
+                        error={this.state.error}
+                        helperText={this.state.helper}
+                        defaultValue={this.default}
+                    />
+
+                    <Button variant='contained' onClick={this.submitCode}>
+                        Submit
+                    </Button>
+                </Paper>
+
+                <Dialog
+                    open={this.state.open}
+                    onClose={this.closePopup}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description"
+                >
+                    <DialogTitle id="alert-dialog-title">{this.state.message.title}</DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description">
+                            {this.state.message.text}
+                        </DialogContentText>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button onClick={this.closePopup} color="primary" autoFocus>
+                            Ok
+                        </Button>
+                    </DialogActions>
+                </Dialog>
+            </React.Fragment>
+        );
+    }
 }
 
-export default App;
+export default withStyles(AppStyles)(App);
