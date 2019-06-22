c8331f4ac8dfb5e38d35cd5567d780a9868989df
ChosenQuill <31707957+ChosenQuill@users.noreply.github.com>
2019-06-22 04:37:34 -0500

    Updates

--- Config.js
+++ Config.js
@@ -7,6 +7,3 @@
 // Change the server address to whatever website you will be hosting the server on. The default is set to the same computer (localhost). Make sure you have the protocol "http://", port 5000 (:5000), and the ending slash (/).
 // In the end, it should look like "http://example.com:5000/" or "http://127.0.0.1:5000/".
 var serverAddress = "http://localhost:5000/";
-
-// Change the basename to whatever you want the subfolder of your website to be. The default is /Discardo, or the default subdirectory when you fork the Discardo repository.
-var baseName = "/Discardo";
