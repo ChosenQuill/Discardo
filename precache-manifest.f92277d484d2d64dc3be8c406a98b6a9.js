98d913006348b288bde373c7415b073eb844a5a0
SureDroid <support@suredroid.com>
2019-07-28 16:49:09 -0500

    Updates

--- precache-manifest.f92277d484d2d64dc3be8c406a98b6a9.js
+++ precache-manifest.f92277d484d2d64dc3be8c406a98b6a9.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "16bc2ce7a7afb29bbf67be4fea6fd7eb",
+    "revision": "e81f0a4182b4c87f4dbfee144ff299e7",
     "url": "./index.html"
   },
   {
-    "revision": "bcfc97974d623d22c195",
+    "revision": "26afaf3e751470ead257",
     "url": "./static/css/main.19e0f645.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "./static/js/2.371a8a9d.chunk.js"
   },
   {
-    "revision": "bcfc97974d623d22c195",
-    "url": "./static/js/main.a88f7787.chunk.js"
+    "revision": "26afaf3e751470ead257",
+    "url": "./static/js/main.68058099.chunk.js"
   },
   {
     "revision": "8c97409f0ee389fe75da",
