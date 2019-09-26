de73404011fa69f7e79a884bb545e50b277924f3
ChosenQuill <31707957+ChosenQuill@users.noreply.github.com>
2019-09-25 23:56:36 -0500

    Updates

--- Config.js
+++ Config.js
@@ -7,3 +7,6 @@
 // Change the server address to whatever website you will be hosting the server on. The default is set to the same computer (localhost). Make sure you have the protocol "http://", port 5000 (:5000), and the ending slash (/).
 // In the end, it should look like "http://example.com:5000/" or "http://127.0.0.1:5000/".
 var serverAddress = "http://localhost:5000/";
+
+// This is the subfolder the project is in. In github page's case, the repository name. If you renamed the repository to something else, please change it here. The default repo name is Discardo.
+var baseName = "Discardo";
