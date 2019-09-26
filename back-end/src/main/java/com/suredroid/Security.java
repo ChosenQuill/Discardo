d7d6db03df1c4e90a1fa92c8a82d681b51ac9295
SureDroid <support@suredroid.com>
2019-09-26 00:39:38 -0500

    WIP Trying SecurityManager

---
+++ back-end/src/main/java/com/suredroid/Security.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2019 SureDroid. Published under the GNU General Public Use License. See LICENSE.MD for more information.
+ */
+
+package com.suredroid;
+
+public class Security extends SecurityManager {
+    
+}
